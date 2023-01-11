bfdd2166224667e31d0c2a5754eb26785698a3b5
soryeon kim <kimsr9210@naver.com>
2023-01-11 13:25:39 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n) {
+    	int[] answer = new int  [(n % 2 == 1 ?  n /2 + 1 : n / 2)];
+		int cnt = 0; 
+		for(int i = 0; i <= n ; i++) { 
+			if(i % 2 == 1) { 
+			    answer[cnt] = i; 
+			    cnt ++; 
+			} 
+		}
+        return answer;
+    }
+}
