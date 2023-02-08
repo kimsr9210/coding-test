70d84fc172f7f61d47cf9d09a9f1a4cc29d7f04b
kimsr9210 <kimsr9210@naver.com>
2023-02-08 14:23:44 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.35 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -6,7 +6,6 @@ class Solution {
         //공백제거 후 쉼표로 치환 
         //쉼표를 기준으로 문자를 분리하여 배열에 저장
         for(int i = 0; i < arr.length; i++) {
-        	//System.out.println(arr[i]);
         	if(!arr[i].equals("Z")) { //Z가 아니라면
             	answer += Integer.parseInt(arr[i]);
         	}else { //Z라면
