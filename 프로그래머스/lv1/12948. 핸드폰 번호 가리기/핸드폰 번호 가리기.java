c05e17a56627697a5737272cd023e6f2237e5ffd
kimsr9210 <kimsr9210@naver.com>
2023-02-15 11:23:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 9.71 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        char[] c = phone_number.toCharArray(); //문자열을 한 글자씩 쪼개서 char 배열에 넣어줌
+        for(int i = 0; i < c.length-4; i++) {
+        	c[i] = '*';
+        } 
+        for(int i = 0; i < c.length; i++) {
+        	answer += c[i];
+        }
+        return answer;
+    }
+}
