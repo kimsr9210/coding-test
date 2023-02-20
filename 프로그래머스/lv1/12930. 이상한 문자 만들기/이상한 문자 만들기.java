455ec3e9e69adb82360d79704379f3c67ee92f9a
kimsr9210 <kimsr9210@naver.com>
2023-02-20 16:44:19 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.08 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        s = s.toLowerCase();   
+        int cnt = 0;   
         String[] str = s.split(""); 
         for(int i = 0; i < str.length; i++) {
-        	if(i % 2 == 0) { //짝수
-        		str[i] = str[i].toUpperCase();
-        	}
+        	cnt = (str[i].equals(" ")) ? 0 : cnt+1;
+        	str[i] = (cnt % 2 == 0) ? str[i].toLowerCase() : str[i].toUpperCase();
         	answer += str[i];
         }
         return answer;
