e0a207aff04117882ad1a204064e5f252748dd30
kimsr9210 <kimsr9210@naver.com>
2023-02-22 15:45:04 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.31 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i = 0; i < numbers.length; i++) {
+        	for(int j = i+1; j < numbers.length; j++) {
+        		if(!arrayList.contains(numbers[i] + numbers[j])) {
+        			arrayList.add(numbers[i] + numbers[j]);
+        		}
+        	}
+        }
+        int[] answer = new int[arrayList.size()];
+        for(int i = 0; i < arrayList.size(); i++) {
+        	answer[i] = arrayList.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
