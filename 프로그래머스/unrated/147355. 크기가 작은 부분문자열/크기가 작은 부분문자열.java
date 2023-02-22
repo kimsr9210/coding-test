46c407c8325c854e8b03174393156e349721c506
kimsr9210 <kimsr9210@naver.com>
2023-02-22 16:46:24 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.37 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -6,7 +6,10 @@ class Solution {
         ArrayList<String> arrayList = new ArrayList<>();
         for(int i = 0; i <= t.length() - p.length(); i++) {
         	arrayList.add(t.substring(i, i+p.length()));
-        	if(Long.parseLong(arrayList.get(i)) <= Long.parseLong(p)) answer++;
+        	//Integer.ParseInt 했더니 런타임에러가 떴다.
+        	//String은 18자리 까지 될 수 있음  -> String을 int로 변환시 자릿수 오류
+        	//Long.parseLong으로 변환
+        	if(Long.parseLong(arrayList.get(i)) <= Long.parseLong(p)) answer++; 
         }
         return answer;
     }
