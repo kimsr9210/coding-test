5719ab8b7a48f8c9e64afa1ae1950578127665aa
kimsr9210 <kimsr9210@naver.com>
2023-02-27 16:21:18 +0900

    [level 1] Title: 2016년, Time: 37.55 ms, Memory: 93.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,17 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+class Solution {
+    public String solution(int a, int b) {
+        Calendar calendar = Calendar.getInstance(); //날짜 시간 가져오기
+        //SimpleDateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일 E요일");  //5월 24일
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd (E)", Locale.ENGLISH);  //날짜 포맷 변환
+        calendar.set(Calendar.YEAR, 2016);
+        calendar.set(Calendar.MONTH, a-1);
+        calendar.set(Calendar.DATE, b);
+        String  answer = dateFormat.format(calendar.getTime()); //날짜 가져옴 => 2022-05-24 (Tue)
+        System.out.println(answer);
+        answer = answer.substring(12, 15).toUpperCase();
+        return answer;
+    }
+}
