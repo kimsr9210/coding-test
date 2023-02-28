495d72543f0f836e81ab49eeb7683b82a10fc89a
kimsr9210 <kimsr9210@naver.com>
2023-02-28 15:10:04 +0900

    Update 소수 찾기.java

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,15 +1,18 @@
 class Solution {
     public int solution(int n) {
-        boolean[] check = new boolean[n+1]; //배열 길이 6
         //Arrays.fill(check, true); //배열의 값을 true로 초기화
+    	
+    	//제곱근이란?
+    	//간단히 말해 제곱의 반대 개념
+    	//제곱근 메소드 Math.sqrt()
         int answer = 0;
-        for(int i = 2; i <= n; i++){ //반복 2 3 4 5
-            if(!check[i]){  //false 일때 
-                for(int j = i + i; j <= n; j+=i){  //4
-                    check[j] = true;
-                }
-                answer++;
-            }
+        for(int i = 2; i <= n; i ++) { //2,3,4,5
+        	int cnt = 0;
+        	for(int j = 2; j <= (int)Math.sqrt(i); j++) {
+        		if(i % j == 0) cnt++;
+        		if(cnt == 1) break;
+        	}
+        	if(cnt == 0) answer++;
         }
         return answer;
     }
