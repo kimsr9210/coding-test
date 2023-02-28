8d5781f9ac8839d5dec918164127a7410d2b563a
kimsr9210 <kimsr9210@naver.com>
2023-02-28 16:27:36 +0900

    [level 1] Title: 모의고사, Time: 0.90 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] answers) {
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+        int[] student1 = {1, 2, 3, 4, 5}; //5
+        int[] student2 = {2,1,2,3,2,4,2,5}; //8
+        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10
+        //1,2,3,4,5
+        for(int i = 0 ; i < answers.length; i++) {
+        	if(answers[i] == student1[i % 5]) {
+        		cnt1++;
+        	}
+        	if(answers[i] == student2[i % 8]) {
+        		cnt2++;
+        	}
+        	if(answers[i] == student3[i % 10]) {
+        		cnt3++;
+        	}
+        }
+
+        //Math.max() : 더 큰수를 찾는 함수
+        int max = Math.max(Math.max(cnt1,cnt2),cnt3); 
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        if(max == cnt1) arrayList.add(1);
+        if(max == cnt2) arrayList.add(2);
+        if(max == cnt3) arrayList.add(3);
+        
+        int[] answer = new int [arrayList.size()];
+        
+        for(int i = 0; i < arrayList.size(); i++) {
+        	answer[i] = arrayList.get(i);
+        }
+        return answer;
+    }
+}
