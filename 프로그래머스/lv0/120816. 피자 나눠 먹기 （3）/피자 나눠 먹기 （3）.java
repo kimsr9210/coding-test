9a80e1cf661779a53961844e5ab2284b2a7ddc54
kimsr9210 <kimsr9210@naver.com>
2023-03-02 14:37:58 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int slice, int n) {
+    	//(/ -> 일반적인 나누기 연산, % -> 나누기를 한 후 나머지를 구하는 연산 )
     	int answer = (n % slice == 0) ? n / slice : n / slice +1;
         return answer;
     }
