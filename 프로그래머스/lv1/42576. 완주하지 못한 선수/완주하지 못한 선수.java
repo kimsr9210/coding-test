6414c230cb6d21c58693ec054b14948c79ce3d5d
kimsr9210 <kimsr9210@naver.com>
2023-03-07 17:42:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.55 ms, Memory: 96.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        
+        for(String p : participant) {
+        	hashMap.put(p, hashMap.getOrDefault(p, 0)+1);
+        }
+        
+        for(String c : completion) {
+        	hashMap.put(c, hashMap.get(c) -1);
+        }
+        
+        for(String key : hashMap.keySet()) {
+        	if(hashMap.get(key) != 0) { 
+        		answer = key;
+        		break;
+        	}
+        }
+        return answer;
+    }
+}
