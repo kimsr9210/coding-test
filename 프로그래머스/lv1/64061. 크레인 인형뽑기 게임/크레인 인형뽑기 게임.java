949bd3ef0705c453d07809c13ef4f96b6ae36a8d
kimsr9210 <kimsr9210@naver.com>
2023-03-09 14:36:15 +0900

    Update 크레인 인형뽑기 게임.java

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 class Solution {
     public int solution(int[][] board, int[] moves) {
-                int answer = 0; //인형을 제거하는 횟수
+         int answer = 0; //인형을 제거하는 횟수
         
         //Stack() 자료구조
         //한 쪽 끝에서만 자료를 넣고 뺄 수 있는 LIFO(Last In First Out) 형식의 자료 구조
