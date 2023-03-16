a8c57770f214fb48731fb1897c82a4a453280097
kimsr9210 <kimsr9210@naver.com>
2023-03-16 16:30:27 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -5,15 +5,14 @@ class Solution {
         int[] answer = { 0, 0 };
         int width = board[0] / 2 ;
         int height = board[1] / 2 ;
-     
-        for(int i = 0; i < keyinput.length; i ++) {
-        	if(keyinput[i].equals("up") && height > answer[1]) {
+        for(String input : keyinput){
+        	if(input.equals("up") && height > answer[1]) {
         		answer[1] += 1;
-        	}else if(keyinput[i].equals("down")  && -height < answer[1] ) {
+        	}else if(input.equals("down")  && -height < answer[1] ) {
         		answer[1] -= 1;
-        	}else if(keyinput[i].equals("left") && -width < answer[0]) {
+        	}else if(input.equals("left") && -width < answer[0]) {
         		answer[0] -= 1; 
-        	}else if(keyinput[i].equals("right") && width > answer[0]) {
+        	}else if(input.equals("right") && width > answer[0]) {
         		answer[0] += 1;
         	}
         }
