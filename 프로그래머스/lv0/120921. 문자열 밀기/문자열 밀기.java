17cc248ebd3d98254afdf5df785bce4fae566c47
kimsr9210 <kimsr9210@naver.com>
2023-03-20 16:36:01 +0900

    [level 0] Title: 문자열 밀기, Time: 1.14 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = -1;
+        String cut = "";
+                if(A.equals(B)) {
+        	answer = 0 ; 
+        	System.out.println(answer);
+        	return answer;
+        }else {
+            for(int i = 0 ; i < A.length(); i++) {
+                cut = A.substring(A.length()-1,A.length());
+                A = A.substring(0, A.length()-1);
+                A = cut + A ;
+                if(A.equals(B)) {
+                	answer = i+1; 
+                	return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
