f8e9b3a2996293955fcb7cf64daef1c3a062bacc
kimsr9210 <kimsr9210@naver.com>
2023-04-06 16:34:26 +0900

    Update 그대로 출력하기.java

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class Main{
 	public static void main(String args[]){
-        Scanner scan = new Scanner(System.in);
-        while(scan.hasNextLine()) { 
-            //nextLine() : 문자 또는 엔터를 치기 전까지의 문장 전체를 입력받는다.
-        	String str = scan.nextLine(); 
-        	System.out.println(str);
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNextLine()) { 
+		    //nextLine() : 문자 또는 엔터를 치기 전까지의 문장 전체를 입력받는다.
+			String str = scan.nextLine(); 
+			System.out.println(str);
+		}
         }
-	}
 }
