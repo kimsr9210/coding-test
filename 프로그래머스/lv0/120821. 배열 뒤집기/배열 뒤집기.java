9dbd077bc9885435868e54672e09b73682568d86
kimsr9210 <kimsr9210@naver.com>
2023-04-14 16:42:42 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,13 +1,13 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list) {
-    	int[] answer =  new int [num_list.length]; //5
-    	int idx = 0;
-
-    	for(int i = num_list.length-1; i >= 0 ; i--) {
-    		answer[idx] = num_list[i];
-    		System.out.println(answer[idx]);
-    		idx++;
-    	}
+        int[] answer = new int [num_list.length]; 
+        int idex = 0;
+        for(int i = num_list.length-1; i >= 0; i--){
+            answer[idex] = num_list[i] ;
+            idex++;
+        }
         return answer;
     }
 }
