21455ee002edc0b6583c460482a591b5f6b93bda
kimsr9210 <kimsr9210@naver.com>
2023-04-20 14:04:09 +0900

    Update 최소직사각형.java

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -2,13 +2,14 @@ import java.util.TreeSet;
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
+        //데이터 중복x 오름차순으로 정렬
         TreeSet<Integer> lTreeSet = new TreeSet<>(); //왼쪽
         TreeSet<Integer> rTreeSet = new TreeSet<>(); //오른쪽
         for(int i = 0; i < sizes.length; i++){
             lTreeSet.add(Math.max(sizes[i][0], sizes[i][1]));
             rTreeSet.add(Math.min(sizes[i][0], sizes[i][1]));
         }
-        answer = lTreeSet.last() * rTreeSet.last();
+        answer = lTreeSet.last() * rTreeSet.last(); //마지막 인덱스 끼리 곱한다.
         return answer;
     }
 }
