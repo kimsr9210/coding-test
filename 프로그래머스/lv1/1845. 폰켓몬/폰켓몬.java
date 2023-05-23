15152c95fc733aabb45117fbf063eab936c64e7c
kimsr9210 <kimsr9210@naver.com>
2023-05-23 10:44:15 +0900

    [level 1] Title: 폰켓몬, Time: 2.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,6 +1,9 @@
 import java.util.HashSet;
 class Solution {
     public int solution(int[] nums) {
+        //1. 기존 length를 구한다.
+        //2. 중복값을 제거한 length를 구한다.
+        //3. 두 값중 최소값이 정답.
         HashSet<Integer> hashSet = new HashSet<>(); //순서유지 x 중복 불가
         for(int i = 0; i < nums.length; i++) {
             if(!hashSet.contains(nums[i])) hashSet.add(nums[i]);
