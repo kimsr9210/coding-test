307d819e3f4603c9e4e1a66478d39d5aef08a610
kimsr9210 <kimsr9210@naver.com>
2023-05-25 17:44:40 +0900

    [level 2] Title: N개의 최소공배수, Time: 8.69 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] arr) {
         int answer = 0;
-        int num = arr[arr.length-1]; //제일 마지막 index의 숫자
+        int num = arr[arr.length-1]; //제일 마지막 index 배수
         int ok = arr.length - 1; //총 통과해야 하는 갯수
         while(ok != answer) {
         	answer = 0;
