76849c74de6569a19dc381faaae94805d27f3c14
kimsr9210 <kimsr9210@naver.com>
2023-06-12 13:18:46 +0900

    [unrated] Title: l로 만들기, Time: 20.59 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,13 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for(int i = 0; i < myString.length(); i++){
-            int num = myString.charAt(i);
-            if(num < 108) num = 108;
-            char ch = (char)num;
-            answer += String.valueOf(ch);
-        }
-        System.out.println(answer);
+        String answer = myString.replaceAll("[^l-z]", "l");
         return answer;
     }
 }
