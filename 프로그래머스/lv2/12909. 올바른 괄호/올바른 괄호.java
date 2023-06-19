e3389892fe9bf996538f1e21a8118334821a9c0d
kimsr9210 <kimsr9210@naver.com>
2023-06-19 10:54:32 +0900

    [level 2] Title: 올바른 괄호, Time: 10.10 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,18 @@
+import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        char[] str = s.toCharArray();
         int open = 0;
-        for(int i = 0; i < s.length(); i++) {
-        	if(str[0] != '(') {
-        		answer = false;
-        		break;
-        	}else if(open == -1) {
-        		answer = false;
-        		break;
-        	}else if(str[i] == '(') {  //괄호를 열은 만큼 닫아야함
-        		open++;
-        	}else {
-        		open--;
-        	}
+        Stack<String> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(0) == ')' || open == -1){
+                answer = false;
+                break;
+            }else if(s.charAt(i) == '('){
+                open++;
+            }else if(s.charAt(i) == ')'){
+                open--;
+            }
         }
         if(open != 0) answer = false;
         return answer;
