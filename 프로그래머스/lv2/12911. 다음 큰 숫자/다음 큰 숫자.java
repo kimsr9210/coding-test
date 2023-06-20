dcfef98966f26daf0e67241e15edfacb51fb4948
kimsr9210 <kimsr9210@naver.com>
2023-06-20 13:19:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(int n) {
-    	//Integer.bitCount()
-    	//파라미터에 int형 정수가 들어갔을때
-    	//정수의 이진수 ex)13 => 1101에서 1의 개수인 3을 리턴 해준다.
-        int answer = Integer.bitCount(n); //이진수에서 1의 개수를 세어주는 함수
-        while(true) {
-        	n++;
-        	if(answer == Integer.bitCount(n)) {
-        		answer = n;
-        		return answer;
-        	}
-        }     
+        int answer = Integer.bitCount(n);
+        while(true){
+            n++;
+            if(answer == Integer.bitCount(n)){ //이진수에 1의 개수를 세어주는 함수
+                answer = n;
+                return answer;
+            }
+        }
     }
 }
