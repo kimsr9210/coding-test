a8ecb7aebec222f1f79ee2510a2bf4ce6941024b
kimsr9210 <kimsr9210@naver.com>
2023-06-21 14:17:39 +0900

    [level 2] Title: 숫자의 표현, Time: 1.50 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        for(int i = 1; i <= n; i++) {
-        	int sum = i ;
-        	for(int j = i + 1 ; j <= n; j++) {
-        		sum += j;
-        		if(sum == n) {
-        			answer ++;
-        			break;
-        		}else if(sum > n ){
-        			break;
-        		}
-        	}
+        int answer = 0;
+        for(int i = 1; i <= n; i++){
+            int sum = 0;
+            for(int j = i; j <= n; j++){
+                sum += j;
+                if(sum == n){
+                    answer++;
+                    break;
+                } else if(sum > n){
+                    break;
+                }
+            }
         }
         return answer;
     }
