11922b0772b77d127526e80b9340d9f5c70d9e3d
kimsr9210 <kimsr9210@naver.com>
2023-06-21 17:42:48 +0900

    [level 2] Title: 짝지어 제거하기, Time: 47.88 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,16 +1,17 @@
 import java.util.Stack;
-class Solution {
+class Solution{
     public int solution(String s){
-    	int answer = 0; ////성공1 , 실패 0
-    	Stack<Character> stack = new Stack<>();
-    	for(char c : s.toCharArray()) {
-    		if(!stack.isEmpty() && stack.peek() == c) { //stack에 값이 있고 , 맨 위의 원소와 c 가 같다면,
-    			stack.pop(); //맨 위에 원소 제거
-    		}else {
-    			stack.push(c); //삽입
-    		}
-    	}
-    	answer = (stack.isEmpty()) ? 1 : 0 ;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(stack.isEmpty()){
+                stack.push(s.charAt(i));
+            }else if(stack.peek() == s.charAt(i)){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(i));
+            } 
+        }
+        int answer = stack.size() == 0 ? 1 : 0;
         return answer;
     }
 }
