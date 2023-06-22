3c6707b8b20086b3b072a6f350245ad469080f61
김소련 <kimsr9210@thekeytech.co.kr>
2023-06-22 14:26:03 +0900

    level 0

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(int a, int b) {
-        String plus = String.valueOf(a) + String.valueOf(b); 
-        int mltpl = 2 * a * b;
-        if(Integer.parseInt(plus) == mltpl) return a+b;
-        int answer = Math.max(Integer.parseInt(plus),mltpl);
-        return answer;
-    }
-}
