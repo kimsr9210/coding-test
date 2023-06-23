dd582ee548d0e2cde9153bd3a60d9662630dfa1d
kimsr9210 <kimsr9210@naver.com>
2023-06-23 16:15:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,11 +2,11 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = {0,0}; 
+        
         HashSet<String> hashSet = new HashSet<>();
         hashSet.add(words[0]);
         
         String before = words[0].substring(words[0].length()-1, words[0].length());
-        
         for(int i = 1; i < words.length; i++) {
         	if(!hashSet.contains(words[i])) {
         		hashSet.add(words[i]);
@@ -26,7 +26,6 @@ class Solution {
         	before = words[i].substring(words[i].length()-1, words[i].length());
             
         }
-        
         return answer;
     }
 }
