68281c2bd9a1ff0e09641c29a06cd06cb05f0f32
김소련 <kimsr9210@thekeytech.co.kr>
2023-06-28 14:42:30 +0900

    가장 큰 수

---
+++ 내일배움코스/Step1_3.java
@@ -0,0 +1,36 @@
+package codingTestSkills;
+
+import java.util.Arrays;
+
+public class Step1_3 {
+
+	public static void main(String[] args) {
+		Step1_3 step1_3 = new Step1_3(); 
+
+		int[] numbers = {3, 30, 34, 5, 9};
+		step1_3.solution(numbers);
+		
+	}
+
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] str = new String[numbers.length];
+        
+        for(int i = 0; i < str.length; i++) {
+        	str[i] = String.valueOf(numbers[i]); //숫자를 문자형으로 변환해서 대입
+        }
+        
+        //내림차순 정렬
+        //람다표현식
+        //문자열 b+a , a+b 를 비교하여 결과에 따라 순서를 결정
+        Arrays.sort(str, (a, b) -> (b + a).compareTo(a + b));
+        
+        for(String s : str) {
+        	answer += s;
+        } 
+        
+        if(str[0].equals("0")) answer = "0";
+        
+        return answer;
+    }
+}
