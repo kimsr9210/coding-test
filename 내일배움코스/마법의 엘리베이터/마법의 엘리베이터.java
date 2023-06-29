2d7d19e25dd48e2e4f0b95b2d5a8d683b6e2f629
김소련 <kimsr9210@thekeytech.co.kr>
2023-06-29 13:34:48 +0900

    가장 큰 수

---
+++ 내일배움코스/마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,29 @@
+package codingTestSkills;
+
+public class Step3_2 {
+	public static void main(String[] args) {
+		Step3_2 step3_2 = new Step3_2(); 
+
+		int storey = 2554;
+		step3_2.solution(storey);
+		
+	}
+
+    public int solution(int storey) {
+        return elevator(storey);
+    }
+    
+    public int elevator (int floor) {
+    	
+    	if(floor <= 1) { //c1 끝, c2
+    		return floor; 
+    	}
+    	
+    	int under10 = floor % 10; //나머지 6 , 2 
+    	int rest = floor / 10; //몫 1
+    	
+    	int c1 = under10 + elevator(rest); //6 + 1
+    	int c2 = (10 - under10) + elevator(rest + 1); //10 - 6 = 4; , 2
+    	return Math.min(c1,c2);
+    }
+}
