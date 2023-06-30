a091ad29edbdcd8ab7394a50eba6228bb0962a95
kimsr9210 <kimsr9210@naver.com>
2023-06-30 14:10:41 +0900

    Update 예산.java

--- 내일배움코스/예산/예산.java
+++ 내일배움코스/예산/예산.java
@@ -1,9 +1,9 @@
 package codingTestSkills;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Step1_3 {
-	//예산(진행중(틀림))
+	//예산
 	public static void main(String[] args) {
 		Step1_3 step1_3 = new Step1_3(); 
 
@@ -12,47 +12,29 @@ public class Step1_3 {
 		step1_3.solution(budgets, M);
 		
 	}
-
-    int answer = 0;
-    int cnt = 0; //평균보다 작은 수 카운트
-    public int solution(int[] budgets, int M) {
-        Arrays.sort(budgets);
-
-        int max = budgets[budgets.length-1]; //가장 큰수 150
-        int avg = M / budgets.length; //평균 예산 121
-        while(true) {
-        	budget(budgets, avg, cnt, answer);
-        	
-        	System.out.println("avg : " + avg);
-        	System.out.println("cnt : " + cnt);
-        	System.out.println("max : " + max);
-        	max -= 1; 
-        	int sum = (max * cnt) + answer;
-        	System.out.println("sum : " + sum);
-        	if(sum < M) { //M = 485
-        		
-        		for(int i = 0; i < cnt; i++) {
-        			if(budgets[i] > max) { //평균 예산 보다 크면
-        	        	avg -= 1;
-        				break;
-        			}
-        		}
-        		answer = max;
-        		System.out.println("answer :::::::::::::::::::::: " + answer);
-        		return answer;
-        	}
-        }
-    }
-    
-    public void budget (int[] budgets, int avg, int cnt, int answer) {
-    	this.cnt = 0;
-    	this.answer = 0;
-        for(int b : budgets) {
-        	if(avg < b) { 
-        		this.cnt++;
-        	}else {
-        		this.answer+= b;
-        	}
-        }
-    }
+	
+	public int solution(int[] budgets, int M) {
+		//최소값, 최대값 ,중간값 구하기
+		int answer = 0;
+		int min = 0; //최소값
+		int max = IntStream.of(budgets).max().orElse(0); // //높은 값이 없다면 0을 반환
+		
+		while(min <= max) {
+			int mid = (min + max) / 2; //중간값
+			
+			int sum = 0;
+			for(int b : budgets) {
+				if(b > mid) sum += mid;
+				else sum += b;
+			}
+			
+			if(sum <= M) {
+				min = mid + 1;
+				answer = mid;
+			}else {
+				max = mid - 1;
+			}
+		}
+		return answer ;
+	}
 }
