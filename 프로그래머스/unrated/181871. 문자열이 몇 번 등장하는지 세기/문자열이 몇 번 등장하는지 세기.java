b982c2253b43aafbb5b6e9722ff31e9144411815
kimsr9210 <kimsr9210@naver.com>
2023-07-03 10:40:27 +0900

    [unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.08 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
-        int patIdex = pat.length();
-        for(int i = 0; i <= myString.length() - pat.length(); i++){
-            if(myString.substring(i,patIdex).contains(pat)) answer++;
-            patIdex++;
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.substring(i).startsWith(pat)) answer++;
         }
         return answer;
     }
