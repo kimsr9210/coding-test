4ffd4c2ade120fb9ef8b527ea7097089b51c6c4c
김소련 <kimsr9210@thekeytech.co.kr>
2023-07-04 09:42:25 +0900

    (중간고사) 티셔츠

--- 내일배움코스/(중간고사) 티셔츠/티셔츠.java
+++ 내일배움코스/(중간고사) 티셔츠/티셔츠.java
@@ -7,13 +7,32 @@ public class Step2_2 {
 	public static void main(String[] args) {
 		Step2_2 step2_2 = new Step1_3(); 
 
-		int[] numbers = {3, 30, 34, 5, 9};
-		step1_2.solution(numbers);
+		int[] people = {1, 2, 3, 4, 6}; //참가자의 상의 크기
+		int[] tshirts = {1, 1, 1, 6}; //주최가 가진 티셔츠 크기
+		step2_2.solution(people, tshirts);
 		
 	}
 
     public int solution(int[] people, int[] tshirts) {
         int answer = 0;
+        
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        
+        for(int i = 0; i < tshirts.length; i++) {
+        	int value = 1;
+        	if(hashMap.containsKey(tshirts[i])) value = hashMap.get(tshirts[i]) + 1;
+        	hashMap.put(tshirts[i], value);
+        }
+        System.out.println(hashMap);
+        for(int i = 0; i < people.length; i++) {
+        	//티셔츠의 사이즈가 있으면서, 재고가 0이 아닐때
+        	if(hashMap.containsKey(people[i]) && hashMap.get(people[i]) > 0) { 
+        		hashMap.put(people[i], hashMap.get(people[i])-1);
+        		answer++;
+        	}
+        }
+        System.out.println(hashMap);
+        System.out.println(answer);
         return answer;
     }
 
