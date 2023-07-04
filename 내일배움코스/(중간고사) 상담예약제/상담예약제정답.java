de7277eca753cff7f8a1540681ff279f2781ce79
kimsr9210 <kimsr9210@naver.com>
2023-07-04 14:24:08 +0900

    Update 상담예약제정답.java

--- 내일배움코스/(중간고사) 상담예약제/상담예약제정답.java
+++ 내일배움코스/(중간고사) 상담예약제/상담예약제정답.java
@@ -1,51 +1,78 @@
 package codingTestSkills;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+class Customer {
+    final int arrivedAt;
+    final String name;
 
+    Customer(String name, String arrivedAt) {
+        this.name = name;
+        this.arrivedAt = parseTime(arrivedAt);
+    }
 
-//중간고사 1. 상담예약제
+    private int parseTime(String time) {
+        // 시간(String)을 분(Int)으로 변경
+        String[] s = time.split(":");
+        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
+    }
+}
 
-public class Step2_1 {
+class Solution {
+    public String[] solution(String[][] booked, String[][] unbooked) {
+        // Customer 객체로 변환
+        Queue<Customer> bookedCustomers = new LinkedList<>();
+        for (String[] customer : booked) {
+            bookedCustomers.offer(new Customer(customer[1], customer[0]));
+        }
+        Queue<Customer> unbookedCustomers = new LinkedList<>();
+        for (String[] customer : unbooked) {
+            unbookedCustomers.offer(new Customer(customer[1], customer[0]));
+        }
 
-	public static void main(String[] args) {
-		Step2_1 step2_1 = new Step2_1(); 
+        List<String> answer = new LinkedList<>();
 
-		//String[][] booked = {{"09:10","lee"},{"12:10","kim"}};
-		String[][] booked = {{"09:10","lee"}};
-		String[][] unbooked = {{"09:00","kim"},{"14:07","bae"}};
-		
-		//String[][] booked = {{"09:55","hae"}, {"10:05","jee"}};
-		//String[][] unbooked = {{"10:04","hee"},{"14:07","eom"}};
-		step2_1.solution(booked, unbooked);
-		
-	}
+        // 시간의 시작은 먼저 온 고객의 도착시간
+        int currentTime = Math.min(bookedCustomers.peek().arrivedAt, unbookedCustomers.peek().arrivedAt);
 
-    public String[] solution(String[][] booked, String[][] unbooked) {        
-    	String[] answer = new String [booked.length + unbooked.length]; //3개
-    	
-    	HashMap<Integer, String> hashMap = new HashMap<>();
-    	ArrayList<Integer> arrayList = new ArrayList<>();
-    	
-    	for(int i = 0; i < booked.length; i++) {
-    		int time = Integer.parseInt(booked[i][0].substring(0,2) + booked[i][0].substring(3,5)); 
-    		hashMap.put(time, booked[i][1]);
-    		arrayList.add(time);
-    	}
+        while (!bookedCustomers.isEmpty() || !unbookedCustomers.isEmpty()) {
+            if (bookedCustomers.isEmpty()) {
+                // 예약된 고객이 없는 경우 예약되지 않은 고객을 모두 추가하고 종료
+                for (Customer c : unbookedCustomers) answer.add(c.name);
+                break;
+            }
+            if (unbookedCustomers.isEmpty()) {
+                // 예약되지 않은 고객이 없는 경우 예약된 고객을 모두 추가하고 종료
+                for (Customer c : bookedCustomers) answer.add(c.name);
+                break;
+            }
 
-    	for(int i = 0; i < unbooked.length; i++) {
-    		int time = Integer.parseInt(unbooked[i][0].substring(0,2) + unbooked[i][0].substring(3,5)); 
-    		hashMap.put(time, unbooked[i][1]);
-    		arrayList.add(time);
-    	}
-    	
-    	Collections.sort(arrayList); 
- 
-    	for(int i = 0; i < answer.length; i++) { 
-    		answer[i] = hashMap.get(arrayList.get(i)); 
-    		System.out.println(answer[i]);
-    	}
-        return answer;
+            final Customer bookedCustomer = bookedCustomers.peek();
+            final Customer unbookedCustomer = unbookedCustomers.peek();
+
+            if (currentTime >= bookedCustomer.arrivedAt) {
+                // 현재시간이 예약된 고객의 도착시간 이후하면 예약 고객을 바로 상담
+                answer.add(bookedCustomers.poll().name);
+                currentTime += 10; // 다음 평가시간은 상담이 종료되는 10분 후
+            } else if (currentTime >= unbookedCustomer.arrivedAt) {
+                // 현재시간이 예약되지 않은 고객의 도착시간 이후하면 비예약 고객을 바로 상담
+                answer.add(unbookedCustomers.poll().name);
+                currentTime += 10; // 다음 평가시간은 상담이 종료되는 10분 후
+            } else {
+                Customer c;
+                // 예약/비예약 고객 중 먼저 온 고객을 상담하고
+                if (bookedCustomer.arrivedAt < unbookedCustomer.arrivedAt) {
+                    c = bookedCustomers.poll();
+                } else {
+                    c = unbookedCustomers.poll();
+                }
+                answer.add(c.name);
+                // 다음 평가시간은 고객의 상담이 종료되는 도착시간의 10분 후
+                currentTime = c.arrivedAt + 10;
+            }
+        }
+        System.out.println(answer.toArray(new String[0]));
+        return answer.toArray(new String[0]);
     }
 }
