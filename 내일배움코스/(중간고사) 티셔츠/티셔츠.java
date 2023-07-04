544874ddae24216d41ee5faeb796c2b3ad8e1d48
김소련 <kimsr9210@thekeytech.co.kr>
2023-07-04 09:34:57 +0900

    (중간고사) 티셔츠

---
+++ 내일배움코스/(중간고사) 티셔츠/티셔츠.java
@@ -0,0 +1,17 @@
+package codingTestSkills;
+
+import java.util.Arrays;
+
+public class Step2_2 {
+
+	public static void main(String[] args) {
+		Step2_2 step2_2 = new Step1_3(); 
+
+		int[] numbers = {3, 30, 34, 5, 9};
+		step1_2.solution(numbers);
+		
+	}
+
+
+    
+}
