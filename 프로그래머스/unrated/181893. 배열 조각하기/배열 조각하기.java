48bbc120d0df769a44b56130ef177dcba2a0a9bc
kimsr9210 <kimsr9210@naver.com>
2023-07-05 10:24:00 +0900

    Update 배열 조각하기.java

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        int[] answer = {};
         //.copyOf() //원본배열, 복사할 길이
         //.copyOfRange() //원본배열, 복사할 시작인덱스, 복사할끝인덱스
         for(int i = 0; i < query.length; i++){
