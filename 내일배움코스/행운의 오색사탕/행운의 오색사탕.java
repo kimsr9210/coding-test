a3fbced40a9ae3f2bfc0197cce1731e81fedfa4e
kimsr9210 <kimsr9210@naver.com>
2023-07-12 15:35:35 +0900

    Update 행운의 오색사탕.java

--- 내일배움코스/행운의 오색사탕/행운의 오색사탕.java
+++ 내일배움코스/행운의 오색사탕/행운의 오색사탕.java
@@ -1,18 +1,37 @@
 package codingTestSkills;
 
-import java.util.*;
-
 //행운의 오색사탕
-public class Step3_2 {
+public class Step3_5 {
 	public static void main(String[] args) {
-		Step3_2 step3_1 = new Step3_2(); 
+		Step3_5 step3_5 = new Step3_5(); 
 
-		String[][] clothes = {{"yellow_hat","headgear"},{"blue_sunglasses","eyewear"},{"green_turban","headgear"}};
-		//String[][] clothes = {{"crow_mast","face"},{"blue_sunglasses","face"},{"smoky_makeup","face"}};
-		step3_1.solution(clothes);
-    }
+		String candy = "RYRYRGPRYRYRBB";
+		int[] positions = {12, 1, 14, 7};
+		
+		//String candy = "BPBRBPBRB";
+		//int[] positions = {3, 6, 9}; //	[1, 1, 2]
+		step3_5.solution(candy, positions);
+		
+	}
+	
+    public int[] solution(String candy, int[] positions) {
+        int[] answer = new int [positions.length];
 
-    public int solution(String[][] clothes) {
+        for(int i = 0 ; i < positions.length; i++) {
+        	if(positions[i] == 1) continue;
+        	
+        	String str = candy.substring(0, positions[i]);
+
+        	int each = str.length() / 2;
+        	if(str.length() % 2 != 0) each = each + 1;
+
+        	for(int j = 0; j < each; j++) {
+        		String cut1 = str.substring(0, j+1);
+        		String cut2 = str.substring(str.length()-(j+1), str.length());
+
+        		if(cut1.equals(cut2)) answer[i] = answer[i]+1;
+        	}
+        }
         return answer;
     }
 }
