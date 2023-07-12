9b05663f167a692542040cab7233fe3bbb056290
김소련 <kimsr9210@thekeytech.co.kr>
2023-07-12 14:13:22 +0900

    행운의 오색사탕

---
+++ 내일배움코스/행운의 오색사탕/행운의 오색사탕.java
@@ -0,0 +1,18 @@
+package codingTestSkills;
+
+import java.util.*;
+
+//행운의 오색사탕
+public class Step3_2 {
+	public static void main(String[] args) {
+		Step3_2 step3_1 = new Step3_2(); 
+
+		String[][] clothes = {{"yellow_hat","headgear"},{"blue_sunglasses","eyewear"},{"green_turban","headgear"}};
+		//String[][] clothes = {{"crow_mast","face"},{"blue_sunglasses","face"},{"smoky_makeup","face"}};
+		step3_1.solution(clothes);
+    }
+
+    public int solution(String[][] clothes) {
+        return answer;
+    }
+}
