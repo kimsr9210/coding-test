30bb60e27054ad542c9fa5e6d2cefbae7ed9f185
kimsr9210 <kimsr9210@naver.com>
2023-07-13 10:31:13 +0900

    Update 두 수의 합.java

--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
@@ -1,6 +1,8 @@
 import java.math.BigInteger;
 class Solution {
     public String solution(String a, String b) {
+        //import java.math.BigInteger;
+    	//BigInteger가 문자열로 되어 있기 때문에 초기화하기 위해서는 문자열을 인자 값으로 넘겨주어야 한다.
         String answer = "";
         BigInteger x = new BigInteger(a);
         BigInteger y = new BigInteger(b);
