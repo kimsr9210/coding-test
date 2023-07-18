3f2edc72cf7578981724f0780f9a9c02d6b124c3
김소련 <kimsr9210@thekeytech.co.kr>
2023-07-18 10:28:32 +0900

    내일배움 코스 기말고사

---
+++ 내일배움코스/판데믹/Step4_1.java
@@ -0,0 +1,20 @@
+package codingTestSkills;
+
+//판데믹
+public class Step4_1 {
+	public static void main(String[] args) {
+		Step4_1 step4_1 = new Step4_1(); 
+
+		int rows = 3;
+		int columns = 4;
+		int max_virus = 2;
+		int[][] queries = {{3,2},{3,2},{2,2},{3,2},{1,4},{3,2},{2,3},{3,1}};
+		step4_1.solution(rows, columns, max_virus, queries);
+		
+	}
+	
+    public int[][] solution(int rows, int columns, int max_virus, int[][] queries) {
+        int[][] answer = {};
+        return answer;
+    }
+}
