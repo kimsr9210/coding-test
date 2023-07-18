fd0482631c5c673f887630b7f5e5588177b44525
김소련 <kimsr9210@thekeytech.co.kr>
2023-07-18 15:46:53 +0900

    시험

---
+++ 내일배움코스/(기말고사) 선후수 수강/선후수 수강.java
@@ -0,0 +1,23 @@
+package codingTestSkills;
+
+import java.util.Stack;
+
+//선후수 수강
+public class Step4_2 {
+	public static void main(String[] args) {
+		Step4_2 step4_2 = new Step4_2(); 
+
+		String[] s1 = {"A", "E", "B", "D", "B", "H", "F", "H", "C"};
+		String[] s2 = {"G", "C", "G", "F", "J", "E", "B", "F", "B"};
+		String k = "B";
+		//["D", "H", "E", "C", "F", "B"]
+		
+		step4_2.solution(s1, s2, k);
+		
+	}
+	
+    public String[] solution(String[] s1, String[] s2, String k) {
+        String[] answer = {};
+        return answer;
+    }
+}
