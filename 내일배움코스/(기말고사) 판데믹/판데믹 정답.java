1d7aa032336aa906f40a6ceed13852d9be5af15c
kimsr9210 <kimsr9210@naver.com>
2023-07-18 16:14:15 +0900

    Update 판데믹 정답.java

--- 내일배움코스/(기말고사) 판데믹/판데믹 정답.java
+++ 내일배움코스/(기말고사) 판데믹/판데믹 정답.java
@@ -2,7 +2,29 @@ package codingTestSkills;
 
 import java.util.Arrays;
 
-//판데믹(수정중)
+class Cell {
+    int row;
+    int col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return row == cell.row && col == cell.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+}
+
 public class Step4_1 {
 	public static void main(String[] args) {
 		Step4_1 step4_1 = new Step4_1(); 
@@ -14,58 +36,52 @@ public class Step4_1 {
 		step4_1.solution(rows, columns, max_virus, queries);
 		
 	}
+	/*
+	(1)지문 이해 및 풀이 계획
+	r행과 c열을 2차원 배열에 표현하기 위해서 인덱스로 변경(-1) 해주는 것을 주의하세요
 	
-    public int[][] solution(int rows, int columns, int max_virus, int[][] queries) {
-        int[][] answer = new int [rows][columns];
-        
-        System.out.println(-1 > 0);
-        System.out.println(-1 < 0);
-        
-        for(int i = 0; i < queries.length; i++) {
-        	int row = queries[i][0]-1;
-        	int column = queries[i][1]-1;
-        	//System.out.print(queries[i][0]);
-        	//System.out.print(", ");
-        	//System.out.println(queries[i][1]);
-        	if(answer[row][column] == max_virus) { //현재 위치에서 상,하,좌 ,우 세균증식
-            	System.out.println("현재 위치 row : " + row);
-            	System.out.println("현재 위치 column : " + column);
-        		int [][] arr = {{row-1,column},{row+1,column},{row,column-1},{row,column+1}};
-        		System.out.println(Arrays.deepToString(arr));
-        		
-        		for(int j = 0; j < arr.length; j++) {
-        			if(arr[j][0] < rows || arr[j][1] < column) { //값이 벗어나지 않으면
-        				if(arr[j][0] > 0 || arr[j][1] > 0) {
-            				System.out.println("값이 벗어나지 않으면");
-            				if(answer[arr[j][0]][arr[j][1]] != max_virus) { //작으면
-            					System.out.println("answer 현재값 : " + answer[arr[j][0]][arr[j][1]] );
-            					answer[arr[j][0]][arr[j][1]] = answer[arr[j][0]][arr[j][1]]+1;
-            					System.out.println("answer 더한값 : " + answer[arr[j][0]][arr[j][1]] );
-            				}
-        				}
-        			}
-        		}
-        		
+	연쇄적으로 주변에 증식되는 것을 BFS 방식으로 계산할 수 있습니다.
+	
+	DFS로 증식을 계산해도 동일한 결과를 얻을 수 있습니다.
+	*
+	*/
 
-        		/*
-        		answer[row-1][column] = answer[row-1][column]+1; //상
-        		answer[row+1][column] = answer[row+1][column]+1; //하
-        		answer[row][column-1] = answer[row+1][column]+1; //좌
-        		answer[row][column+1] = answer[row+1][column]+1; //우
-        		*/
-        	}else {
-        		answer[row][column] = answer[row][column]+1;
-        	}
-        }
-        
-        for(int i = 0; i < answer.length; i++) {
-        	System.out.println("");
-            for(int j = 0; j < answer[i].length; j++) {
-            	System.out.print(answer[i][j]);
-            	System.out.print(", ");
+
+	
+public int[][] solution(int rows, int columns, int max_virus, int[][] queries) {
+        int[][] answer = new int[rows][columns];
+        Queue<Cell> queue = new LinkedList<>();
+
+        for (int[] q : queries) {
+            // 방문한 곳을 확인하기 위해 Set 을 사용
+            // (Cell 이 hashCode, equals 를 재정의 하고 있기 때문에 가능)
+            Set<Cell> visited = new HashSet<>();
+
+            // 세균을 증식했을 때 연쇄적으로 주변에 증식되는 것을 BFS적으로 계산
+            queue.offer(new Cell(q[0] - 1, q[1] - 1)); // 인덱스로 만들기 위해 -1
+            while (!queue.isEmpty()) {
+                Cell current = queue.poll();
+
+                // 영역 밖이면 무시
+                if (current.row < 0 || current.row >= rows || current.col < 0 || current.col >= columns) continue;
+                // 이미 방문한 곳이면 무시
+                if (visited.contains(current)) continue;
+
+                visited.add(current);
+                if (answer[current.row][current.col] < max_virus) {
+                    answer[current.row][current.col] += 1;
+                    continue;
+                }
+
+                // 주변에 증식
+                queue.offer(new Cell(current.row, current.col + 1));
+                queue.offer(new Cell(current.row, current.col - 1));
+                queue.offer(new Cell(current.row + 1, current.col));
+                queue.offer(new Cell(current.row - 1, current.col));
             }
         }
-        System.out.println("여기왓어?");
+
+
         return answer;
     }
 }
