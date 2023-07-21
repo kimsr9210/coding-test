55aa57ddd6a552cc231d25b230edf5f5c7279027
kimsr9210 <kimsr9210@naver.com>
2023-07-21 10:22:03 +0900

    [unrated] Title: 세로 읽기, Time: 1.33 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        for(int i = 0; i < my_string.length() / m; i++){
+            answer += my_string.substring(c-1,c);
+            c = c + m;
+        }
+        return answer;
+    }
+}
