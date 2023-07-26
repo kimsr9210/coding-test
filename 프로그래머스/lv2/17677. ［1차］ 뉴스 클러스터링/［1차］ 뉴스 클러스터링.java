9d809fcbb78a79b70a0acc98c90843171df5d7d6
kimsr9210 <kimsr9210@naver.com>
2023-07-26 15:45:14 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.53 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+
+        HashMap<String, Integer> hashMap1 = new HashMap<>();
+        HashMap<String, Integer> hashMap2 = new HashMap<>();
+        for(int i = 0; i < str1.length()-1; i++){
+            String key = str1.substring(i,i+2).replaceAll("[^A-Z]", ""); //대문자로 치환;
+            if(key.length() == 2) hashMap1.put(key, hashMap1.getOrDefault(key,0)+1);
+        }
+
+        for(int i = 0; i < str2.length()-1; i++){
+            String key = str2.substring(i,i+2).replaceAll("[^A-Z]", ""); //대문자로 치환;
+            if(key.length() == 2) hashMap2.put(key, hashMap2.getOrDefault(key,0)+1);
+        }
+
+        if(hashMap1.size() == 0 && hashMap2.size() == 0) return 65536;
+
+        int same = 0; //교집합 inter
+        int notSame = 0; //합집합 union
+        for(String k : hashMap1.keySet()){
+            if(hashMap2.containsKey(k)){
+                same += Math.min(hashMap1.get(k), hashMap2.get(k));
+                notSame += Math.max(hashMap1.get(k), hashMap2.get(k));
+                hashMap2.remove(k);
+            }else{
+                notSame += hashMap1.get(k);
+            }
+        }
+
+        for(String k : hashMap2.keySet()){
+            notSame += hashMap2.get(k);
+        }
+
+        float result = ((float) same / notSame) * 65536;
+        answer = (int) result;
+        return answer;
+    }
+}
