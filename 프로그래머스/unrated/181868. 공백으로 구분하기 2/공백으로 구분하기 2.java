98c09a5d37edc85cfb6308c62e9f86645019947f
kimsr9210 <kimsr9210@naver.com>
2023-07-27 11:22:01 +0900

    Update 공백으로 구분하기 2.java

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,5 +1,7 @@
 class Solution {
     public String[] solution(String my_string) {
+    	//.trim() 왼쪽, 오른쪽 공백 제거
+    	//.split("[ ]+") 문자열을 공백 문자(스페이스)를 기준으로 분할, + 여러번 반복        
         String[] answer = my_string.trim().split("[ ]+");
         return answer;
     }
