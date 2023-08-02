7c0fd17a245b713678c677d16ad7a7c3a51cd215
kimsr9210 <kimsr9210@naver.com>
2023-08-02 16:16:04 +0900

    Update 주식가격.java

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] solution(int[] prices) {
+        //이중 포문으로 푸는 방법
         int[] answer = new int [prices.length];
         for(int i = 0; i < prices.length; i++){
             for(int j = i+1; j < prices.length; j++){
@@ -7,6 +8,28 @@ class Solution {
                 if(prices[i] > prices[j]) break;
             }
         }
+
+        /* [스택으로 푸는 방법]
+        int[] answer = new int [prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < prices.length; i++){
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                answer[stack.peek()] = i - stack.peek(); //index 3
+                stack.pop();
+            }
+            stack.push(i); //0
+        }
+
+        while(!stack.isEmpty()){
+            System.out.println(stack);
+            answer[stack.peek()] = prices.length - stack.peek() - 1; //5 - 4 - 1 => 4 3 1 0
+            stack.pop();
+        }
+        
+        */
+
+        
         return answer;
     }
 }
