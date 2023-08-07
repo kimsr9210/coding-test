5cb0a963293d24b25f1af57bc608a57ff9c41092
kimsr9210 <kimsr9210@naver.com>
2023-08-07 16:02:01 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.75 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -14,13 +14,14 @@ class Solution {
 
         for(int i = 0; i < str2.length()-1; i++){
             String key = str2.substring(i,i+2).replaceAll("[^A-Z]", ""); //대문자로 치환;
-            if(key.length() == 2) hashMap2.put(key, hashMap2.getOrDefault(key,0)+1);
+            //if(key.length() == 2) hashMap2.put(key, hashMap2.getOrDefault(key,0)+1);
+            if(key.length() == 2) hashMap2.merge(key, 1, Integer::sum);
         }
 
         if(hashMap1.size() == 0 && hashMap2.size() == 0) return 65536;
 
-        int same = 0; //교집합
-        int notSame = 0; //합집합
+        int same = 0; //교집합 inter
+        int notSame = 0; //합집합 union
         for(String k : hashMap1.keySet()){
             if(hashMap2.containsKey(k)){
                 same += Math.min(hashMap1.get(k), hashMap2.get(k));
