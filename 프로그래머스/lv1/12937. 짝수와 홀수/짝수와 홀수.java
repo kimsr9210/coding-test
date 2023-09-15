3b1a57c0c525ceede7035b6e8ca72448426a0a6e
kimsr9210 <kimsr9210@naver.com>
2023-09-15 17:15:58 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(int num) {
-        String answer = ""; //커밋테스트
-        answer =  (num % 2 == 0 ? "Even" : "Odd"); 
+        String answer =  (num % 2 == 0 ? "Even" : "Odd"); 
         return answer;
     }
 }
