57f11a864b97fa929bf6756c63f94919da50ab09
김소련 <kimsr9210@thekeytech.co.kr>
2023-09-19 14:26:02 +0900

    lv0

--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("a = "+ sc.nextInt());
-        System.out.println("b = "+ sc.nextInt());
-    }
-}
