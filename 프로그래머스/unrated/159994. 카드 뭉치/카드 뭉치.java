0321194ba4a355ad264b31b4f215e42a4850d335
kimsr9210 <kimsr9210@naver.com>
2023-09-19 16:47:35 +0900

    [unrated] Title: 카드 뭉치, Time: 0.21 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -1,15 +1,13 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Arrays;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
         
-        Queue<String> card1 = new LinkedList<>();
-        Queue<String> card2 = new LinkedList<>();
-        
-        for(int i = 0; i < cards1.length; i++) card1.add(cards1[i]);
-        for(int i = 0; i < cards2.length; i++) card2.add(cards2[i]);
-        
+        Queue<String> card1 = new LinkedList<>(Arrays.asList(cards1));
+        Queue<String> card2 = new LinkedList<>(Arrays.asList(cards2));
+
         for(int i = 0; i < goal.length; i++){
             String c1 = card1.peek();
             String c2 = card2.peek();
