a76c85888479ab3c65ad8d0405b777d2a80199fb
kimsr9210 <kimsr9210@naver.com>
2023-09-20 09:34:56 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
 
 ### 성능 요약
 
-메모리: 79 MB, 시간: 0.10 ms
+메모리: 73.1 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -1,18 +1,8 @@
 class Solution {
     public long solution(int a, int b) {
         long answer = 0;
-        if(a < b) {
-            for(int i = a; i <= b; i++ ) {
-            	answer += i;
-            }
-        }
-        
-        if(a > b) {
-            for(int i = b; i <= a; i++ ) {
-            	answer += i;
-            }
-        }
-      
+        if(a < b) for(int i = a; i <= b; i++ ) answer += i;
+        if(a > b) for(int i = b; i <= a; i++ ) answer += i;
         if(a == b) answer = a;
         return answer;
     }
