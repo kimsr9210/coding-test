6c59ec5a5f1c56ef119dcf6e27db0ebf3aae5dde
kimsr9210 <kimsr9210@naver.com>
2023-09-20 16:16:20 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.51 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,25 +1,22 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer = new int [score.length];
+        int[] answer = new int[score.length];
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
         
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i = 0; i < score.length; i++) {
+            
+            queue.add(score[i]);
         
-        for(int i = 0; i < score.length; i++){
-            if(i < k){
-                arrayList.add(score[i]);
-                arrayList.sort(Collections.reverseOrder());
-                answer[i] = arrayList.get(arrayList.size()-1);
-            }else{
-                //k번째 가수 점수보다 더 높으면
-                if(arrayList.get(k-1) < score[i]){
-                    arrayList.remove(k-1);
-                    arrayList.add(score[i]);
-                }
-                arrayList.sort(Collections.reverseOrder());
-                answer[i] = arrayList.get(k-1);
+            if(queue.size() <= k) {
+                answer[i] = queue.peek();
+            }else {
+                queue.remove();
+                answer[i] = queue.peek();
             }
         }
+
         return answer;
     }
 }
