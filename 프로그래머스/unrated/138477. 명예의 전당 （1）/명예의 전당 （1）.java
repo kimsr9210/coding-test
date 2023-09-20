8e5168ab4d584f4db44967b6110c13fda1dd6c57
kimsr9210 <kimsr9210@naver.com>
2023-09-20 16:18:40 +0900

    Update 명예의 전당 （1）.java

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
+        //queue로 풀었을때
         int[] answer = new int[score.length];
 
         PriorityQueue<Integer> queue = new PriorityQueue<>();
@@ -17,6 +18,28 @@ class Solution {
             }
         }
 
+        //Arraylist로 풀었을때 
+        /*
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++){
+    
+            if(i < k){
+                arrayList.add(score[i]);
+                arrayList.sort(Collections.reverseOrder());
+                answer[i] = arrayList.get(arrayList.size()-1);
+            }
+            
+            if(i >= k){
+                //k번째 가수 점수보다 더 높으면
+                if(arrayList.get(k-1) < score[i]){
+                    arrayList.remove(k-1);
+                    arrayList.add(score[i]);
+                }
+                arrayList.sort(Collections.reverseOrder());
+                answer[i] = arrayList.get(k-1);
+            }
+        }*/
         return answer;
     }
 }
