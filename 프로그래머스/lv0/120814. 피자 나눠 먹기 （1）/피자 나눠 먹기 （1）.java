82fab8a127a98cffa7cc3f30e78c30019a09bb21
kimsr9210 <kimsr9210@naver.com>
2023-09-22 10:14:43 +0900

    Update 피자 나눠 먹기 （1）.java

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = (int) Math.ceil((double)n/7);
+        int answer = (int) Math.ceil((double)n/7); //Math.ceil() 소수값이 존재할때 올림
         return answer;
     }
 }
