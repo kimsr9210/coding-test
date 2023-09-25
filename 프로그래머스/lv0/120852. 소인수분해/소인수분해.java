3b9b184beeed8e68ac6975459cb49ffcd9b1fe11
kimsr9210 <kimsr9210@naver.com>
2023-09-25 13:18:38 +0900

    [level 0] Title: 소인수분해, Time: 1.83 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+
+        HashSet<Integer> set = new HashSet<>(); 
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        while(n != 0) {
+        	for(int i = 2; i <= n; i++) {
+        		if(n % i == 0) {
+        			n = n / i ;
+        			if(!arrayList.contains(i)) arrayList.add(i);
+        			if(n == 1) n = 0;
+        			break;
+        		}
+        	}
+        }
+        
+    	int[] answer = arrayList.stream().mapToInt(i -> i).toArray();
+
+        return answer;
+    }
+}
