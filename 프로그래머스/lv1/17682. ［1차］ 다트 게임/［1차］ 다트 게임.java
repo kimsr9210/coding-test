88cf020b3aabe9195bb06f1ef362cb9bb2a4c532
kimsr9210 <kimsr9210@naver.com>
2023-10-06 15:21:38 +0900

    [level 1] Title: [1차] 다트 게임, Time: 1.19 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        int index = 0;
+        int[] scores = new int [3] ; //점수 배열 저장
+        String score = ""; //점수
+        for(int i = 0; i < dartResult.length(); i++) {
+        	char c = dartResult.charAt(i);
+        	switch(c) {
+        		case 'S' : 
+        			scores[index++] = (int)Math.pow(Integer.parseInt(score), 1);
+        			score = "";
+        			break;
+        		case 'D' : 
+        			scores[index++] = (int)Math.pow(Integer.parseInt(score), 2);
+        			score = "";
+        			break;
+        		case 'T' : 
+        			scores[index++] = (int)Math.pow(Integer.parseInt(score), 3);
+        			score = "";
+        			break;
+        		case '*' : 
+        			scores[index-1] = scores[index-1] * 2;
+        			if(index > 1) scores[index-2] = scores[index-2] * 2;
+        			break;
+        		case '#' : 
+        			scores[index-1] = scores[index-1] - (scores[index-1] * 2);
+        			break;
+        		default : 
+        			score += String.valueOf(c);
+        	}
+        }
+
+        for(int s : scores) answer += s;
+
+        return answer;
+    }
+}
