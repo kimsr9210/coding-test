a3b89e7d642b72e02aa28774577edd967142e091
kimsr9210 <kimsr9210@naver.com>
2023-11-13 15:11:10 +0900

    [level 1] Title: 둘만의 암호, Time: 1.36 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,19 +1,16 @@
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        for(int i = 0; i < s.length(); i++) {
-        	char c = s.charAt(i);
-        	for(int j = 0; j < index; j++) {
-        		c++;
-                if (c > 'z') {
-                    c -= 26;
-                }
-                if (skip.contains(String.valueOf(c))) {
-                    j--;
-                }
-        	}
-        	answer += c;
+        StringBuffer sb = new StringBuffer();
+        for(char letter : s.toCharArray()){
+            char temp = letter;
+            int idx = 0;
+            while(idx < index){
+                temp = temp == 'z' ? 'a' : (char)(temp+1);
+                if(!skip.contains(String.valueOf(temp))) idx++;
+            }
+            sb.append(temp);
         }
+        String answer = sb.toString();
         return answer;
     }
 }
