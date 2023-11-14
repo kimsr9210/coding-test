1002062895893475950b42f133ee6a7016c32f97
김소련 <kimsr9210@naver.com>
2023-11-14 15:42:32 +0900

    lv1

--- 프로그래머스/lv1/176963. 추억 점수/README.md
+++ 프로그래머스/lv1/176963. 추억 점수/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 0.03 ms
+메모리: 78.8 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -12,7 +12,11 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
+
+### 제출 일자
+
+2023년 11월 2일 15:38:37
 
 ### 문제 설명
 
@@ -92,4 +96,4 @@ Empty
 <p>설명 생략</p>
 
 
-> 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+> 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/lv1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/lv1/176963. 추억 점수/추억 점수.java
@@ -1,16 +1,18 @@
-import java.util.HashMap;
+import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int [photo.length];
+        int[] answer = new int [photo.length] ;
         
-        HashMap<String,Integer> hashMap = new HashMap<>();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        
         for(int i = 0; i < name.length; i++){
             hashMap.put(name[i], yearning[i]);
         }
         
-        for(int i = 0 ; i < photo.length; i++){
+        for(int i = 0; i < photo.length; i++){
             for(int j = 0; j < photo[i].length; j++){
-                if(hashMap.containsKey(photo[i][j])) answer[i] += hashMap.get(photo[i][j]);
+                String n = photo[i][j];
+                if(hashMap.containsKey(n)) answer[i] += hashMap.get(n);
             }
         }
         return answer;
