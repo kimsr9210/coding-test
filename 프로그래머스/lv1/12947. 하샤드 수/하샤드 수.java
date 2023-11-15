de79bd8eba53e3bdf479ca64698a4d2c1a999761
kimsr9210 <kimsr9210@naver.com>
2023-11-15 16:59:24 +0900

    Update 하샤드 수.java

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        String str = String.valueOf(x);
         int sum = 0;
-        char[] arr = str.toCharArray(); //String 문자열을 char형 배열로 바꿔서 반환해주는 메서드
+        char[] arr = String.valueOf(x).toCharArray(); 
         for(char c : arr) {
-        	sum += Character.getNumericValue(c); //char를 int로 형변환
+        	//sum += Character.getNumericValue(c); //char를 int로 형변환
+            sum += (int) c - '0';
         }
         if(x % sum != 0) answer = false; 
         return answer;
