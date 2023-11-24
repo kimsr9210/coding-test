5717988a8f22fb20653adbb90febaf9e2d2afe6d
kimsr9210 <kimsr9210@naver.com>
2023-11-24 13:38:45 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.java

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -2,15 +2,11 @@ import java.util.*;
 public class Main{
     public static void main(String srgs[]){
         Scanner sc = new Scanner(System.in);
-		int[] arr = new int[6];
-		int[] chess = {1, 1, 2, 2, 2, 8};
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		for(int i = 0 ; i < arr.length; i++) {
-			arr[i] = chess[i] - arr[i];
-			System.out.println(arr[i]);
-		}
+	int[] arr = new int[6];
+	int[] chess = {1, 1, 2, 2, 2, 8};
+	for(int i = 0; i < arr.length; i++) {
+	    arr[i] = chess[i] - sc.nextInt();
+            System.out.println(arr[i]);
+	}
     }
 }
