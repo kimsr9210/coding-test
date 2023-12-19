c02f20b8d2fef31e219eda6451f7da55abef8699
kimsr9210 <kimsr9210@naver.com>
2023-12-19 15:33:53 +0900

    [level 2] Title: 스킬트리, Time: 8.58 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        char[] skills = skill.toCharArray();
+            for(String s : skill_trees) {
+            	String str = s.replaceAll("[^" + skill + "]", "");
+            	int index = 0;
+            	boolean same = true;
+            	for(int i = 0; i < str.length(); i++) {
+            		char c = str.charAt(i);
+            		if(i == 0 && c != skills[0] || skills[index] != c) {
+            			same = false;
+            			break;
+            		}
+            		if(skills[index] == c) index++;
+            	}
+            	if(same) answer++;
+            }
+
+        System.out.println(answer);
+        return answer;
+    }
+}
