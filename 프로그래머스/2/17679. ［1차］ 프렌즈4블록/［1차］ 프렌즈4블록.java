a8597688643875dd5f465805da0a41c05ec118d1
kimsr9210 <kimsr9210@naver.com>
2024-01-04 15:42:28 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3.37 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        char[][] map = new char [m][n]; //높이 폭
+        
+        for(int i = 0; i < m; i++) {
+        	map[i] = board[i].toCharArray();
+        }
+
+        int answerBefor = -1;
+        boolean[][] isChecked = new boolean[m][n];
+
+        while(true) {
+        	if(answer == answerBefor) break;
+        	answerBefor = answer; //0
+        	
+            //모양이 같은 블록 삭제
+            for(int i = 0; i < m-1; i++) {
+            	for(int j = 0; j < n-1; j++) {
+            		if( map[i][j] != ' ' &&
+            			map[i][j] == map[i][j+1] &&
+            			map[i][j] == map[i+1][j] &&	
+            			map[i][j] == map[i+1][j+1]
+            		) {
+            			isChecked[i][j] = true;
+            			isChecked[i][j+1] = true;
+            			isChecked[i+1][j] = true;
+            			isChecked[i+1][j+1] = true;
+            		}
+            	}
+            }
+            
+            //삭제된 갯수 체크
+            for(int i = 0; i < m; i++) {
+            	for(int j = 0; j < n; j++) {
+            		if(isChecked[i][j]) {
+            			map[i][j] = ' ';
+            			answer++;
+            			isChecked[i][j] = false; //초기화
+            		}
+            	}
+            }
+
+            //교환
+            for(int i = 0; i < m; i++) {
+                for(int ii = m-1; ii > 0; ii--) {
+                	for(int jj = 0; jj < n; jj++) {
+                		if(map[ii][jj] == ' ') {
+                			map[ii][jj] = map[ii-1][jj];
+                			map[ii-1][jj] = ' ';
+                		}
+                	}
+                }
+            }
+        }
+
+        return answer;
+    }
+}
