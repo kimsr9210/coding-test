1e8aa1cd3e9e88fb3ea2400cd31ad63fd10fada5
kimsr9210 <kimsr9210@naver.com>
2024-10-08 14:08:30 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.07 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int min = Math.min(bill[0],bill[1]);
+        int max = Math.max(bill[0],bill[1]);
+        
+        if(wallet[0] >= min && wallet[1] >= max) return 0;
+        if(wallet[1] >= min && wallet[0] >= max) return 0;
+        
+        int answer = 0;
+        while(true){
+            answer++;
+
+            max = max / 2;
+            
+            //지갑에 들어갈 수 있는지 확인.
+            if(wallet[0] >= min && wallet[1] >= max) return answer;
+            if(wallet[1] >= min && wallet[0] >= max) return answer;
+
+            //없으면 다음 로직 실행
+            if(min > max){
+                int divide = max;
+                max = min;
+                min = divide;
+            }
+        }
+    }
+}
