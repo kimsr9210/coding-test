ce23d7aefa1ae72676b2e301b4ea6181f5a80b70
kimsr9210 <kimsr9210@naver.com>
2024-10-11 15:58:14 +0900

    [level 1] Title: 모의고사, Time: 0.98 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,43 +1,26 @@
-import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] answers) {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
-        int[] student1 = {1, 2, 3, 4, 5}; //5개
-        int[] student2 = {2,1,2,3,2,4,2,5}; //8개
-        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10개
-        
-        System.out.println(0 % 5); //-> index : 0
-        System.out.println(1 % 5); //-> index : 1
-        System.out.println(2 % 5); //-> index : 2
-        System.out.println(3 % 5); //-> index : 3
-        System.out.println(4 % 5); //-> index : 4
-        System.out.println(5 % 5); //-> index : 5
-        System.out.println(6 % 5); //-> index : 6
-        for(int i = 0 ; i < answers.length; i++) {
-        	if(answers[i] == student1[i % 5]) {
-        		cnt1++;
-        	}
-        	if(answers[i] == student2[i % 8]) {
-        		cnt2++;
-        	}
-        	if(answers[i] == student3[i % 10]) {
-        		cnt3++;
-        	}
+       int a [] = {1,2,3,4,5};
+        int b [] = {2, 1, 2, 3, 2, 4, 2, 5};
+        int c [] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int[] student = {0,0,0};
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == a[i % 5]) student[0]++;
+            if(answers[i] == b[i % 8]) student[1]++;
+            if(answers[i] == c[i % c.length]) student[2]++;
         }
 
-        //Math.max() : 더 큰수를 찾는 함수
-        int max = Math.max(Math.max(cnt1,cnt2),cnt3); 
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        if(max == cnt1) arrayList.add(1);
-        if(max == cnt2) arrayList.add(2);
-        if(max == cnt3) arrayList.add(3);
-        
-        int[] answer = new int [arrayList.size()];
-        
-        for(int i = 0; i < arrayList.size(); i++) {
-        	answer[i] = arrayList.get(i);
+        int max = Math.max(student[0],(Math.max(student[1],student[2])));
+        int num = 0;
+        for(int i = 0; i < student.length; i++){
+            if(student[i] == max) num++;
+        }
+
+        int index = 0;
+        int[] answer = new int[num];
+        for(int i = 0; i < student.length; i++){
+            if(student[i] == max) answer[index++] = i+1;
         }
         return answer;
     }
