7c4d29859da6347f3dd51c0ba0494a5e0feb8c16
kimsr9210 <kimsr9210@naver.com>
2024-10-15 11:01:20 +0900

    [level 1] Title: 모의고사, Time: 0.94 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,27 +1,32 @@
 class Solution {
     public int[] solution(int[] answers) {
-       int a [] = {1,2,3,4,5};
-        int b [] = {2, 1, 2, 3, 2, 4, 2, 5};
-        int c [] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
+        int[] a = {1, 2, 3, 4, 5}; //5
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5,}; //8
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10;
+        
+        //맞춘갯수 구하기
         int[] student = {0,0,0};
         for(int i = 0; i < answers.length; i++){
-            if(answers[i] == a[i % 5]) student[0]++;
+            if(answers[i] == a[i % 5]) student[0]++; 
             if(answers[i] == b[i % 8]) student[1]++;
-            if(answers[i] == c[i % c.length]) student[2]++;
+            if(answers[i] == c[i % 10]) student[2]++;
         }
-
-        int max = Math.max(student[0],(Math.max(student[1],student[2])));
+        
+        //가장 많이 맞춘 학생 구하기
+        int max = Math.max(student[0],Math.max(student[1],student[2]));
+        
+        //동일한 갯수 맞춘 학생 수 구하기
         int num = 0;
-        for(int i = 0; i < student.length; i++){
-            if(student[i] == max) num++;
+        for(int s : student){
+            if(s == max) num++;
         }
 
-        int index = 0;
-        int[] answer = new int[num];
+        int[] answer = new int [num];
+        int idx = 0;
         for(int i = 0; i < student.length; i++){
-            if(student[i] == max) answer[index++] = i+1;
+            if(student[i] == max) answer[idx++] = i + 1;
         }
+        
         return answer;
     }
 }
