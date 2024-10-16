02f942596a13f1ace483b44e536710bf1eca515b
kimsr9210 <kimsr9210@naver.com>
2024-10-16 15:58:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 22.27 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,18 +1,15 @@
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
-        char[] c = phone_number.toCharArray(); //문자열을 한 글자씩 쪼개서 char 배열에 넣어줌
-        for(int i = 0; i < c.length-4; i++) {
-        	c[i] = '*';
-        } 
-        for(int i = 0; i < c.length; i++) {
-        	answer += c[i];
+        //4자리 빼고 다 별로 바꾸기
+        for(int i = 0; i < phone_number.length(); i++){
+            char c = phone_number.charAt(i);
+            if((phone_number.length() - 4) > i) {
+                answer += '*';
+            }else{
+                answer += c;
+            }
         }
-        
-        //**다른 풀이 방법**
-        //정규식 전방탐색
-        //answer = phone_number.replaceAll(".(?=.{4})", "*");
-        
         return answer;
     }
 }
