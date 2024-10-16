ad4f71b92aad1e8098a608a468e686a939d9164f
kimsr9210 <kimsr9210@naver.com>
2024-10-16 15:39:15 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 10.22 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,24 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-    	//arr 에서 가장 작은 수를 제거한 배열을 리턴
-    	//배열이 빈 배열인 경우엔 배열에 -1 리턴
-        int[] answer = {};
-        int[] copy = arr.clone(); //배열 복사
-        Arrays.sort(copy); //배열 정렬
+        ArrayList<Integer> list = new ArrayList<>();
         
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        if(arr.length == 1) {
-        	arrayList.add(-1);
-        }else {
-            for(int i = 0; i < arr.length; i++) {
-            	if(arr[i] != copy[0]) arrayList.add(arr[i]);
-            }	
+        int min = arr[0];
+        for(int a : arr){
+            list.add(a);
+            if(a <= min) min = a;
         }
-        //Integer list를 배열(array)로 변환
-        answer = arrayList.stream().mapToInt(i -> i).toArray();
+        
+        for(int i = 0; i < list.size(); i++){
+            if(min == list.get(i)) list.remove(i);
+        }
+        
+        if(list.size() == 0){
+            int[] a = {-1};
+            return a;
+        }
+        
+        int[] answer = new int [list.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        
         return answer;
     }
 }
