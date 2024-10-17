ee98cb93156ed19e81f66a90c7d32e27ed126bf2
kimsr9210 <kimsr9210@naver.com>
2024-10-17 15:13:09 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.74 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,19 +1,10 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        
-        //마지막 글자가 공백인지 확인 하기 위해 만든 변수
-        String empty = s.substring(s.length()-1,s.length());
-        String[] str = s.split(" ");
-        
-        for(int i = 0; i < str.length; i ++) {
-        	if(str[i].length() != 0) { //공백이 아니라면
-        		String upper = str[i].substring(0,1).toUpperCase();
-        		String lower =  str[i].substring(1).toLowerCase();
-        		answer += (i != str.length-1 || empty.equals(" ")) ? upper + lower + " " : upper + lower ;
-        	}else { //공백이라면
-        		answer += " ";
-        	}
+        boolean flag = true;
+        for(String ss : s.toLowerCase().split("")){
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
         }
         return answer;
     }
