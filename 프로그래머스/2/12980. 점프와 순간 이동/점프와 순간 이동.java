ae73f08881b99de2e0961433dab371b7a1dc217a
kimsr9210 <kimsr9210@naver.com>
2024-10-17 17:20:13 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,17 +2,7 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-    	//도착지점에서 거꾸로 되감기 해본다고 생각
-    	//2배를 순간이동할 수 있기에 도착하는 지점에서 2로 나누어 되돌아 가는 과정에서는 건전지 사용량이 늘어나지 않음
-        int ans = 0;
-        while(n > 0) {
-        	if(n % 2 == 0) {
-        		n /= 2;
-        	}else {
-        		n--;
-        		ans++;
-        	}
-        }
+        int ans = Integer.bitCount(n);
         return ans;
     }
 }
