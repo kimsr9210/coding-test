52a5e29461bac863aef78d4ea74400e91dc74507
kimsr9210 <kimsr9210@naver.com>
2024-10-18 17:06:58 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,30 +1,25 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {0,0}; 
+        int[] answer = {0,0}; //가장 먼저 탈락하는 사람 번호, 몇번째 차례에 탈락하는지
+
+        HashSet<String> set = new HashSet<>();
         
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.add(words[0]);
+        set.add(words[0]);
+        String lastWord = words[0].substring(words[0].length()-1,words[0].length());
         
-        String before = words[0].substring(words[0].length()-1, words[0].length());
-        for(int i = 1; i < words.length; i++) {
-        	if(!hashSet.contains(words[i])) {
-        		hashSet.add(words[i]);
-        	}else if(hashSet.contains(words[i]) || words[i].length() < 2){
-        		answer[0] = (i % n) + 1;//사람 번호
-        	    answer[1] = (i / n) + 1; //차례
-        	    return answer;
-        	}
-    
-        	String now = words[i].substring(0, 1);
-        	if(!before.equals(now)) {
-        		answer[0] = (i % n) + 1;//사람 번호
-        	    answer[1] = (i / n) + 1; //차례
+        for(int i = 1; i < words.length; i++){
+            
+            String eq = words[i].substring(0,1);
+            
+            if(!set.contains(words[i]) && lastWord.equals(eq) && words[i].length() >= 2){
+                set.add(words[i]);
+                lastWord = words[i].substring(words[i].length()-1,words[i].length());
+            }else{
+                answer[0] = (i % n) + 1;//사람 번호
+                answer[1] = (i / n) + 1;
                 return answer;
-        	}
-            
-        	before = words[i].substring(words[i].length()-1, words[i].length());
-            
+            }
         }
         return answer;
     }
