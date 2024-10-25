068344cf82b672643accba7dabdad1adabdac99e
kimsr9210 <kimsr9210@naver.com>
2024-10-25 14:24:19 +0900

    [level 1] Title: 소수 찾기, Time: 329.61 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int n) {
-        //Arrays.fill(check, true); //배열의 값을 true로 초기화
-    	
     	//제곱근이란?
     	//간단히 말해 제곱의 반대 개념
-    	//제곱근 메소드 Math.sqrt()
+    	//제곱근 메소드 Math.sqrt()        
         int answer = 0;
-        for(int i = 2; i <= n; i ++) { //2,3,4,5
+        for(int i = 2; i <= n; i ++) {
         	int cnt = 0;
         	for(int j = 2; j <= (int)Math.sqrt(i); j++) {
         		if(i % j == 0) cnt++;
