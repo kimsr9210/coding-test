3ffcabcc21f888d6ac1a3cb47eec765f326fdd2b
kimsr9210 <kimsr9210@naver.com>
2024-10-25 13:51:39 +0900

    [level 1] Title: 최소직사각형, Time: 2.86 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,15 +1,12 @@
-import java.util.TreeSet;
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        //데이터 중복x 오름차순으로 정렬
-        TreeSet<Integer> lTreeSet = new TreeSet<>(); //왼쪽
-        TreeSet<Integer> rTreeSet = new TreeSet<>(); //오른쪽
-        for(int i = 0; i < sizes.length; i++){
-            lTreeSet.add(Math.max(sizes[i][0], sizes[i][1]));
-            rTreeSet.add(Math.min(sizes[i][0], sizes[i][1]));
+        int left = 0;
+        int right = 0;
+        for(int card[] : sizes){
+            left = Math.max(left, Math.max(card[0],card[1]));
+            right = Math.max(right, Math.min(card[0],card[1]));
         }
-        answer = lTreeSet.last() * rTreeSet.last(); //마지막 인덱스 끼리 곱한다.
+        int answer = left * right;
         return answer;
     }
 }
