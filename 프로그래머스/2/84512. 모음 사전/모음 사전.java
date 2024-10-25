e668c7b843f41d030a5b0991583f72e0af7b2ee8
kimsr9210 <kimsr9210@naver.com>
2024-10-25 17:13:49 +0900

    [level 2] Title: 모음 사전, Time: 12.23 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,26 +1,25 @@
 import java.util.*;
 class Solution {
+    String[] words = {"A","E","I","O","U"};
     static List<String> list;
-    static String [] words = {"A", "E", "I", "O", "U"};
     public int solution(String word) {
         int answer = 0;
         list = new ArrayList<>();
-        dfs("", 0);
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            if (list.get(i).equals(word)) {
+        dfs("",0);
+        for(int i = 0; i < list.size(); i++){
+            if(word.equals(list.get(i))){
                 answer = i;
                 break;
             }
         }
         return answer;
     }
-
-    static void dfs(String str, int len) {
+    
+    public void dfs(String str, int index){
         list.add(str);
-        if (len == 5) return;
-        for (int i = 0; i < 5; i++) {
-            dfs(str + words[i], len + 1);
+        if(index == words.length) return;
+        for(int i = 0; i < words.length; i++){
+            dfs(str+words[i], index+1);
         }
     }
 }
