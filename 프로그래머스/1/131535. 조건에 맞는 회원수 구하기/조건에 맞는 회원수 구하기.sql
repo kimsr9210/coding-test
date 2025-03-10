f00c0ca9c7b887a3092bdb28af36a6a857420b34
kimsr9210 <kimsr9210@naver.com>
2025-03-10 15:11:19 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+--TO_DATE () : 문자열을 날짜 형식으로 변환
+--TO_DATE('날짜문자열', '날짜형식')
+--TO_DATE('2021-01-01', 'yyyy-mm-dd')
+SELECT COUNT(*)USERS FROM USER_INFO 
+WHERE JOINED BETWEEN TO_DATE('2021-01-01', 'YYYY-MM-DD') AND TO_DATE('2021-12-31', 'YYYY-MM-DD')
+AND AGE BETWEEN 20 AND 29
+;
